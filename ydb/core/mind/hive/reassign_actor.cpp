cd8cc4a9f454c09ed9b0d7f1a432cdc26bf0b394
Alexander Zalyalov <zalyalov@ydb.tech>
2026-05-04 15:38:08 +0000

    clean up after copilot

--- ydb/core/mind/hive/reassign_actor.cpp
+++ ydb/core/mind/hive/reassign_actor.cpp
@@ -131,15 +131,8 @@ public:
     }
 
     void Handle(TEvPrivate::TEvRestartCancelled::TPtr& ev) {
-        if (TLeaderTabletInfo* tablet = Hive->FindTablet(ev->Get()->TabletId)) {
-            auto& actors = tablet->ActorsToNotifyOnRestart;
-            for (auto it = actors.begin(); it != actors.end(); ) {
-                if (*it == SelfId()) {
-                    it = actors.erase(it);
-                } else {
-                    ++it;
-                }
-            }
+        if (TTabletInfo* tablet = Hive->FindTablet(ev->Get()->TabletId)) {
+            std::erase(tablet->ActorsToNotifyOnRestart, SelfId());
         }
         --ReassignInFlight;
         ReassignNextTablet();
